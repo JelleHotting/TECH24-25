1a34e6b21b6653ee062e115db8db944827f032a2
Joepvanderlist <joepvdlist@gmail.com>
2025-04-02 16:07:50 +0200

    fixes begin

---
+++ static/javascript/sanitize.js
@@ -0,0 +1,12 @@
+const xssOptions = {
+    whiteList: {},  // Standaard witlijst: alles is geblokkeerd
+    stripIgnoreTag: true, // Verwijder tags die niet op de witlijst staan
+    onIgnoreTag: function(tag, html) {
+        return '';  // Verwijder ongewenste tags
+    }
+};
+
+// Functie om invoer te saneren met XSS filtering
+function sanitizeInput(input) {
+    return filterXSS(input, xssOptions);
+}
